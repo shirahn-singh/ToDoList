c95a57a93a3e7796724584fbb1332f9b353cb732
Shirahn <22980644@sun.ac.za>
2025-05-08 22:24:27 +0200

    Added styling files and migrated over some styles to confirm if it works

--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import React from "react";
 import ToDoListItem from "./ToDoListItem";
-
+import styles from "../styles/ToDoList.module.css";
 function ToDoList({items, DeleteItem, ToggleComplete}) {
     return (
         <>
-            <div className='items'>
+            <div className= {styles.items}>
                 {
                     items.map((item) => (
                        <ToDoListItem key={item.id} item={item} onDelete={DeleteItem} onComplete={()=>{ToggleComplete(item.id)}}/>
--- src/Components/ToDoListItem.jsx
+++ src/Components/ToDoListItem.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import styles from '../styles/ToDoListItem.module.css';
+
 
 
 function ToDoListItem({ item, onDelete, onComplete }) {
     return (
-        <div key={item.id} className= {item.completed? "to-do-list-item-completed" : 'to-do-list-item'}>
+        <div key={item.id} className= {item.completed? styles.toDoListItemCompleted: styles.toDoListItem}>
             <div>{item.text}</div>
             <button onClick ={onComplete}> {!item.completed ? "Done" : "Whoops"}</button>
             <button onClick={()=>{onDelete(item.id)}}>Delete</button>
