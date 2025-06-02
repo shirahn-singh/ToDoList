0a783b3865020f74343272ed52217401e6d0841a
Shirahn <22980644@sun.ac.za>
2025-06-02 21:22:51 +0200

    Put key info in an env file and used aliases for key values in firebase.js file

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD5lPEPZ7XQN08IAW2l0xQ7ISXTe8oogoA",
-    authDomain: "todoornottodo-20904.firebaseapp.com",
-    projectId: "todoornottodo-20904",
-    storageBucket: "todoornottodo-20904.firebasestorage.app",
-    messagingSenderId: "811980423854",
-    appId: "1:811980423854:web:67d83d273d604ec925233f",
-    measurementId: "G-QXKKPYN63J"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID // optional
   };
   
 
